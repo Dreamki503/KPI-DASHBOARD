1270cdc29f3138650b69db887ac07fe246c4847a
Aryan Leon Jude Mechery <aryanleon503@gmail.com>
2024-10-04 18:52:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://Aryan:College123@127.0.0.1/kpi"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
